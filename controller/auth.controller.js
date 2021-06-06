558a9e62af1f80acd57bcdd85e160d95cdb21719
Ayomide Popoola <popoolaayomide37@gmail.com>
2021-06-05 23:11:14 -0700

    authorization done but not yet tested, waiting for calculation details

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,7 +2,7 @@ const User = require('../model/user');
 const ApiError = require('../utils/apiError');
 const jwt = require('jsonwebtoken');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -28,7 +28,7 @@ exports.register = async (req, res, next) => {
     res.status(200).json({
       status: 'success',
       message: user,
-    //   token
+      //   token
     });
   } catch (error) {
     next(error);
@@ -43,12 +43,42 @@ exports.login = async (req, res, next) => {
       return next(new ApiError('user does not exist'));
     }
 
-    let token = signToken(user._id)
+    let token = signToken(user._id);
     res.status(200).json({
       status: 'success',
       message: user,
       token,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.authorization = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return next(new ApiError('please login to get access'));
+    }
+
+    let decode = await jwt.verify(token, process.env.JWT_SECRET);
+
+    let currentUser = await User.findById({ _id: decode.id });
+
+    if (!currentUser) {
+      return next(new ApiError('unuthorized user', 401));
+    }
+
+    req.user = currentUser;
+    next();
   } catch (error) {
     next(error);
   }
